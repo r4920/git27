0d2d10be313dd206b1ed3658e925753e10f22a4a
r4920 <hr_105@yopmail.com>
2022-03-24 11:41:09 +0000

    Code Synced by DhiWise

---
+++ database/seeders/DefaultUserSeeder.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class DefaultUserSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return  void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+
+        $inputUser = [
+            'username' => $faker->word,
+            'name' => $faker->word,
+            'is_active' => $faker->boolean(true),
+            'created_at' => $faker->dateTime(),
+            'updated_at' => $faker->dateTime(),
+            'email' => 'easton.daniel@gmail.com',
+            'password' => Hash::make('A0qSeL3_Ol77aZf'),
+            'email_verified_at' => Carbon::now(),
+            'user_type' => User::TYPE_USER
+        ];
+
+        $userUser  = User::create($inputUser);
+        $roleUser  = Role::whereName('User')->first();
+        $userUser->assignRole($roleUser);
+
+        $inputAdmin = [
+            'username' => $faker->word,
+            'name' => $faker->word,
+            'is_active' => $faker->boolean(true),
+            'created_at' => $faker->dateTime(),
+            'updated_at' => $faker->dateTime(),
+            'email' => 'monserrate21@hotmail.com',
+            'password' => Hash::make('EhodgpnccLbQJ0j'),
+            'email_verified_at' => Carbon::now(),
+            'user_type' => User::TYPE_ADMIN
+        ];
+
+        $userAdmin  = User::create($inputAdmin);
+        $roleAdmin  = Role::whereName('Admin')->first();
+        $userAdmin->assignRole($roleAdmin);
+
+        $roleSystemUser  = Role::whereName(User::DEFAULT_ROLE)->first();
+        $inputSystemUser = [
+            'name' => $faker->word,
+            'is_active' => $faker->boolean(true),
+            'created_at' => $faker->dateTime(),
+            'updated_at' => $faker->dateTime(),
+            'username' => 'berneice15',
+            'email' => 'concepcion51@hotmail.com',
+            'password' => Hash::make('06bl&lt;`+'),
+            'email_verified_at' => Carbon::now(),
+            'user_type' => User::TYPE_ADMIN
+        ];
+
+        $systemUser = User::create($inputSystemUser);
+        $systemUser->assignRole($roleSystemUser);
+
+        $permissions = Permission::all();
+        $roleSystemUser->givePermissionTo($permissions);
+    }
+}
